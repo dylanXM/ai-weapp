490fbc503b1ebb350e4713e05197e01ef9593efd
dylan_xm <dylan_xm@163.com>
2024-04-19 09:24:12 +0800

    feat: 首页设为聊天页面

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -8,7 +8,7 @@ import { getUserInfo } from '../../api/index';
 Component({
   behaviors: [storeBindingsBehavior],
   data: {
-    activeNav: 'presets',
+    activeNav: 'chat',
   },
   // @ts-ignore
   storeBindings: {
