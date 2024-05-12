27cc8e55e9418c14164f5865994fe0f36222ec05
dylan_xm <dylan_xm@163.com>
2024-05-12 11:49:46 +0800

    feat: 更新模型介绍文案

--- miniprogram/utils/model.ts
+++ miniprogram/utils/model.ts
@@ -14,6 +14,36 @@ const defaultConfig = {
   rounds:8
 };
 
+export const ModelsMapDesc = (key: number, deductType: number, deduct: number) => {
+  const desc = {
+    1: {
+      1: `系统内置模型：支持智能对话，速度更快，每次消耗基础模型 ${deduct} 积分`,
+      2: `系统内置模型：拥有更强的推理能力，每次消耗高级模型 ${deduct} 积分`
+    },
+    2: {
+      1: `百度千帆大模型：每次消耗基础模型 ${deduct} 积分`,
+      2: `百度千帆大模型：每次消耗高级模型 ${deduct} 积分`,
+    },
+    3: {
+      1: `清华智谱大模型：每次消耗基础模型 ${deduct} 积分`,
+      2: `清华智谱大模型：每次消耗高级模型 ${deduct} 积分`,
+    },
+    4: {
+      1: `系统模型：每次消耗基础模型 ${deduct} 积分`,
+      2: `系统模型：每次消耗高级模型 ${deduct} 积分`,
+    },
+    5: {
+      1: `联网模型：支持联网查询总结，每次消耗基础模型 ${deduct} 积分`,
+      2: `联网模型：支持联网查询总结，每次消耗高级模型 ${deduct} 积分`,
+    },
+    6: {
+      1: `全能模型：支持联网查询总结、绘画，每次消耗基础模型 ${deduct} 积分`,
+      2: `全能模型：支持联网查询总结、绘画，每次消耗高级模型 ${deduct} 积分`,
+    }
+  };
+  return desc[key][deductType];
+};
+
 export const formatModelOptions = (modelMaps: ModelData['modelMaps'], chooseModel: ChooseModel) => {
   const options: any[] = [];
   const modelTypes = Object.keys(modelMaps || {});
@@ -25,7 +55,7 @@ export const formatModelOptions = (modelMaps: ModelData['modelMaps'], chooseMode
       const option = {
         name: modelName,
         // subname: `基于模型${model}训练，每次消耗${deductType === 1 ? '基础' : '高级'}模型 ${deduct} 积分`,
-        subname: `每次消耗${deductType === 1 ? '基础' : '高级'}模型 ${deduct} 积分`,
+        subname: ModelsMapDesc(key, deductType, deduct),
         ..._model,
         color: isCurrentModel ? '#07c160' : '#323233',
         className: isCurrentModel ? 'p-chat-model-active-item' : '',
